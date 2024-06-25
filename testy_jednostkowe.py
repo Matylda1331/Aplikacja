ff7cf726f0ac6b823f0a351acadd885fa14e3b23
zostepien <282254@student.pwr.edu.pl>
2024-06-25 16:50:41 +0200

    Update testy_jednostkowe.py

--- testy_jednostkowe.py
+++ testy_jednostkowe.py
@@ -6,17 +6,17 @@ class TestWordleGame(unittest.TestCase):
         self.game = WordleGame()
 
     def test_wszystko_dobrze(self):
-        self.game.haslo = "APPLE"
-        proba = "APPLE"
+        self.game.haslo = "CLOUD"
+        proba = "CLOUD"
         zielone, zolte, szare = self.game.sprawdzanie(proba)
         self.assertEqual(len(zielone), 5)
         self.assertEqual(len(zolte), 0)
         self.assertEqual(len(szare), 0)
-        self.assertIn(('A', 0), zielone)
-        self.assertIn(('P', 1), zielone)
-        self.assertIn(('P', 2), zielone)
-        self.assertIn(('L', 3), zielone)
-        self.assertIn(('E', 4), zielone)
+        self.assertIn(('C', 0), zielone)
+        self.assertIn(('L', 1), zielone)
+        self.assertIn(('O', 2), zielone)
+        self.assertIn(('U', 3), zielone)
+        self.assertIn(('D', 4), zielone)
     
     def test_zla_litera(self):
         self.game.haslo = "APPLE"
@@ -33,16 +33,16 @@ class TestWordleGame(unittest.TestCase):
     
     def test_brak_zielonych(self):
         self.game.haslo = "APPLE"
-        proba = "PALMS"
+        proba = "PINKY"
         zielone, zolte, szare = self.game.sprawdzanie(proba)
         self.assertEqual(len(zielone), 0)
-        self.assertEqual(len(zolte), 3)
-        self.assertEqual(len(szare), 2)
-        self.assertIn(('A', 1), zolte)
+        self.assertEqual(len(zolte), 1)
+        self.assertEqual(len(szare), 4)
         self.assertIn(('P', 0), zolte)
-        self.assertIn(('L', 2), zolte)
-        self.assertIn(('M', 3), szare)
-        self.assertIn(('S', 4), szare)
+        self.assertIn(('I', 1), szare)
+        self.assertIn(('N', 2), szare)
+        self.assertIn(('K', 3), szare)
+        self.assertIn(('Y', 4), szare)
     
 
 if __name__ == "__main__":
